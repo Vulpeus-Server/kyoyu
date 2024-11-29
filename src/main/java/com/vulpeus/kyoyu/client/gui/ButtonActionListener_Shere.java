98f2a41d277528c6b1bb532161b32f5dff7292c4
topi-banana <support@topi-banana.com>
2024-11-29 12:56:11 +0900

    add share button in placement widget

---
+++ src/main/java/com/vulpeus/kyoyu/client/gui/ButtonActionListener_Shere.java
@@ -0,0 +1,36 @@
+package com.vulpeus.kyoyu.client.gui;
+
+//? if client {
+import fi.dy.masa.litematica.schematic.placement.SchematicPlacement;
+import fi.dy.masa.malilib.gui.GuiBase;
+import fi.dy.masa.malilib.gui.Message;
+import fi.dy.masa.malilib.gui.button.ButtonBase;
+import fi.dy.masa.malilib.gui.button.IButtonActionListener;
+
+public class ButtonActionListener_Shere implements IButtonActionListener {
+
+    private final SchematicPlacement schematicPlacement;
+    private final GuiBase messageDisplay;
+
+    public ButtonActionListener_Shere(SchematicPlacement placement, GuiBase messageDisplay) {
+        schematicPlacement = placement;
+        this.messageDisplay = messageDisplay;
+    }
+
+    @Override
+    public void actionPerformedWithButton(ButtonBase buttonBase, int i) {
+        if (!GuiBase.isShiftDown()) {
+            messageDisplay.addMessage(Message.MessageType.ERROR, "kyoyu.error.share_without_shift");
+            return;
+        }
+
+        buttonBase.setEnabled(false);
+
+        // TODO
+        //  `schematicPlacement` add to client registry
+
+        // TODO
+        //  Packet Process on Share scheme
+    }
+}
+//?}
---
+++ src/main/java/com/vulpeus/kyoyu/client/mixins/litematica/WidgetSchematicPlacementMixin.java
@@ -0,0 +1,43 @@
+package com.vulpeus.kyoyu.client.mixins.litematica;
+
+//? if client {
+import com.vulpeus.kyoyu.client.gui.ButtonActionListener_Shere;
+import fi.dy.masa.litematica.gui.widgets.WidgetListSchematicPlacements;
+import fi.dy.masa.litematica.gui.widgets.WidgetSchematicPlacement;
+import fi.dy.masa.litematica.schematic.placement.SchematicPlacement;
+import fi.dy.masa.malilib.gui.button.ButtonGeneric;
+import fi.dy.masa.malilib.gui.widgets.WidgetListEntryBase;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WidgetSchematicPlacement.class)
+public abstract class WidgetSchematicPlacementMixin extends WidgetListEntryBase<SchematicPlacement> {
+
+    @Shadow(remap = false)
+    public int buttonsStartX;
+
+    @Final
+    @Shadow(remap = false)
+    public SchematicPlacement placement;
+
+    protected WidgetSchematicPlacementMixin(int x, int y, int width, int height, @Nullable SchematicPlacement entry, int listIndex) {
+        super(x, y, width, height, entry, listIndex);
+    }
+
+    @Inject(method = "<init>", at = @At("TAIL"), remap = false)
+    public void init(int x, int y, int width, int height, boolean isOdd, SchematicPlacement placement, int listIndex, WidgetListSchematicPlacements parent, CallbackInfo ci) {
+
+        ButtonGeneric shareButton = new ButtonGeneric(buttonsStartX, y + 1, -1, true, "kyoyu.gui.button.share_litematic");
+        // TODO
+        //  Disable when connecting server is not compatible
+        //  shareButton.setEnabled();
+        addButton(shareButton, new ButtonActionListener_Shere(placement, parent.getParentGui()));
+        buttonsStartX = shareButton.getX() - 1;
+    }
+}
+//?}
