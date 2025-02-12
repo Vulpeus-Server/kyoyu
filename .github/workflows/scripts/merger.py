9b1ac019866e022209dc7f2a483a86510e570438
topi-banana <support@topi-banana.com>
2025-02-12 21:28:07 +0900

    MANIFEST.MF merger

--- .github/workflows/scripts/merger.py
+++ .github/workflows/scripts/merger.py
@@ -35,14 +35,28 @@ def main():
         with zipfile.ZipFile(output_path, 'w',
                 compression=zipfile.ZIP_DEFLATED,
                 compresslevel=9) as outfile:
+            manifest: dict = {}
             for jar_path in jars:
                 with zipfile.ZipFile(jar_path, 'r') as infile:
                     for file in infile.namelist():
+                        if file == 'META-INF/MANIFEST.MF':
+                            with infile.open(file, 'r') as rf:
+                                for line in rf.read().decode('utf-8').splitlines():
+                                    if ':' in line:
+                                        k, v = line.split(':', 1)
+                                        manifest[k] = v
+                            continue
                         if file in outfile.namelist():
                             continue
                         with outfile.open(file, 'w') as wf:
                             with infile.open(file, 'r') as rf:
                                 wf.write(rf.read())
+
+            with outfile.open('META-INF/MANIFEST.MF', 'w') as wf:
+                manifest_content = ""
+                for key, value in manifest.items():
+                    manifest_content += f'{key}: {value}\r\n'
+                wf.write(manifest_content.encode('utf-8'))
 
         print(output_path)
 
