e017ac7d5fcf52461794b0f0aad87a506d0c4684
topi-banana <support@topi-banana.com>
2025-02-28 13:16:36 +0900

    remove unused import

--- src/main/java/com/vulpeus/kyoyu/client/mixins/litematica/SchematicPlacementMixin.java
+++ src/main/java/com/vulpeus/kyoyu/client/mixins/litematica/SchematicPlacementMixin.java
@@ -21,7 +21,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
